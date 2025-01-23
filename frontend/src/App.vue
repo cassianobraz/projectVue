46b6373c172283b802822b6bfc585009a2405fdb
cassianobraz <cassanopb@gmail.com>
2025-01-22 22:19:16 -0400

    feat(config inital)

---
+++ frontend/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <header>
+    <Header />
+  </header>
+
+  <main></main>
+</template>
+
+<script setup>
+import Header from './components/Header.vue';
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+}
+header {
+  line-height: 1.5;
+}
+</style>
---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1 class="titulo">{{nome}}</h1>
+  </div>
+</template>
+
+<script setup>
+const nome = 'Cassiano'
+</script>
+
+<style scoped>
+.titulo {
+  margin: 0 auto;
+  text-align: center;
+  color: red;
+}
+</style>
