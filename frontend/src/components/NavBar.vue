134001956a42a606a91246115c2838ab6de5f224
cassianobraz <cassanopb@gmail.com>
2025-01-22 22:36:08 -0400

    feat(component global): Navbar default

---
+++ frontend/src/components/NavBar.vue
@@ -0,0 +1,35 @@
+<template>
+  <nav>
+    <span>Projeto vue</span>
+    <ul>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">Sobre</a></li>
+      <li><a href="#">Login</a></li>
+    </ul>
+  </nav>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+nav {
+  padding: 5px;
+  display: flex;
+  justify-content: space-between;
+  align-content: center;
+  background: #cae7e8;
+}
+ul {
+  display: flex;
+  list-style-type: none;
+}
+ul li {
+  padding: 0 5px;
+}
+ul li a {
+  text-decoration: none;
+  color: #117177
+}
+</style>
